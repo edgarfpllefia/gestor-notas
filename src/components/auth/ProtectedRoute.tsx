64e7477c7d384c76043b158f0394915586d3bc84
arnauagudofpllefia <arnagudo05@gmail.com>
2025-12-30 08:48:39 +0000

    hacer proteccion del router en ProtectedRoute.tsx

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,1 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthContext";
+import { ReactNode } from "react";
 
+interface Props {
+  children: ReactNode;
+  role?: "estudiante" | "admin";
+}
+
+export const ProtectedRoute = ({ children, role }: Props) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (role && user.role !== role) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};

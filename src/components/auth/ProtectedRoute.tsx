c5efe661cf122c858f6fc0c3fcd5f12a878c4df0
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-01-28 17:45:13 +0100

    error protectedRoute solucionado

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "@/contexts/AuthContext"
 
-export default function ProtectedRoute({ children }) {
+export function ProtectedRoute({ children }) {
   const { user } = useAuth()
 
   if (!user) {

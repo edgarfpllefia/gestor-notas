0780b3811f9e5be01c099a1f694c7140fb8be012
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-03-11 13:12:07 +0100

    cambios para conexión con api

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
-import { LoginForm } from "@/components/auth/LoginForm"
+import { LoginForm } from "@/components/auth/LoginForm";
 
 export function LoginPage() {
-  return <LoginForm />
+  return <LoginForm />;
 }
